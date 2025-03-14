9390c257a09fe77a205c2cfd149070d87625e995
NazokatAkhmadjonova00 <nazokatkakhmadjonova@gmail.com>
2025-03-14 14:14:37 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -90,7 +90,12 @@ namespace EudamedAutomation
                 int[] pagesToVisit = { 5, 7, 9, 11, 13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65, 
                     67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99, 101, 103, 105, 107, 109, 111, 113, 115, 117, 119, 
                         121, 123, 125, 127, 129, 131, 133, 135, 137, 139, 141, 143, 145, 147, 149,151, 153, 155, 157, 159, 161, 163, 165,
-                            167, 169, 171, 173, 175, 177, 179, 181, 183, 185, 187, 189, 191, 193, 195, 197, 199, 201, 203, 205, 207, 209, 211, 213, 215, 217}; // Last page is 12 (can be changed)
+                            167, 169, 171, 173, 175, 177, 179, 181, 183, 185, 187, 189, 191, 193, 195, 197, 199, 201, 203, 205, 207, 209, 
+                                211, 213, 215, 217, 217, 219, 221, 223, 225, 227, 229, 231, 233, 235, 237, 239, 241, 243, 245, 247, 249, 
+                                    251, 253, 255, 257, 259, 261, 263, 265, 267, 269, 271, 273, 275, 277, 279, 281, 283, 285, 287, 289, 
+                                        291, 293, 295, 297, 299, 301, 303, 305, 307, 309, 311, 313, 315, 317, 319, 321, 323, 325, 327, 
+                                            329, 331, 333, 335, 337, 339, 341, 343, 345, 347, 349, 351, 353, 355, 357, 359, 361, 363, 
+                                                365, 367, 369, 371, 373, 375, 377, 379, 381, 383, 385, 387, 389, 391, 393, 395, 397, 399}; // Last page is 12 (can be changed)
 
               
                     foreach (int page in pagesToVisit)
@@ -246,7 +251,7 @@ namespace EudamedAutomation
                 int excelRowIndex = 2;
 
 
-                for (int currentPage = 217; currentPage <= totalPages; currentPage++)
+                for (int currentPage = 399; currentPage <= totalPages; currentPage++)
                 {
 
                     var tableRows = driver.FindElements(By.CssSelector("table tbody tr"));
@@ -1386,9 +1391,9 @@ namespace EudamedAutomation
 
                                 // Save the Excel file
                                 Console.WriteLine("Saving the extracted data to an Excel file...");
-                                workbook.SaveAs("Eudamed_Device_Data3_217.xlsx");
+                                workbook.SaveAs("Eudamed_Device_Data_400.xlsx");
 
-                                Console.WriteLine($"Data extraction for a product No {i + 1}! Excel file saved as 'Eudamed_Device_Data3_217.xlsx'.");
+                                Console.WriteLine($"Data extraction for a product No {i + 1}! Excel file saved as 'Eudamed_Device_Data_400.xlsx'.");
 
 
                                 // Wait for the table to reload
